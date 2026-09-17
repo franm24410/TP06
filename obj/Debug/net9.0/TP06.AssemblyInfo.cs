f1ff38dab2d2a466d9e7d078feb794ae7262d55e
FRANCISCO MARTÍNEZ <50235299@ort>
2026-09-17 09:03:08 -0300

    arreglamos minibug en la puerta de una habitacion+

--- obj/Debug/net9.0/TP06.AssemblyInfo.cs
+++ obj/Debug/net9.0/TP06.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP06")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a762a8606e50de53c4573bf9670682adecbe198")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebce68cee3eb333d88011aa2ea77382a3070c126")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP06")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP06")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
