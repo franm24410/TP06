1dea4dd9951161b2306cca25f0c6a9458e267778
L1ROO <654dante@gmail.com>
2026-09-20 15:27:38 -0300

    agregamos puzzle de caida

--- obj/Debug/net9.0/TP06.AssemblyInfo.cs
+++ obj/Debug/net9.0/TP06.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP06")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65a110cb2217e019f69bb04ea2d67c3225d9854b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a7d8aa0941ab9f5c0cee67553a995b30200e90b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP06")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP06")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
