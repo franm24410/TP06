8da1804afc90676bc6aa32c5a3f6e14936684af9
Fran <franm24410@gmail.com>
2026-09-21 00:09:46 -0300

    Añadi la sala del puzzle final

--- obj/Debug/net9.0/TP06.AssemblyInfo.cs
+++ obj/Debug/net9.0/TP06.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP06")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a7d8aa0941ab9f5c0cee67553a995b30200e90b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dea4dd9951161b2306cca25f0c6a9458e267778")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP06")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP06")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
