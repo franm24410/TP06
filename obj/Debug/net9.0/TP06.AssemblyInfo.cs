13bd8a0a62fe99b0f12a1926f458128d609fbe6d
Fran <franm24410@gmail.com>
2026-09-21 18:30:51 -0300

    Añadi la sala del tutorial y los comentarios de los carteles

--- obj/Debug/net9.0/TP06.AssemblyInfo.cs
+++ obj/Debug/net9.0/TP06.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP06")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dea4dd9951161b2306cca25f0c6a9458e267778")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8da1804afc90676bc6aa32c5a3f6e14936684af9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP06")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP06")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
